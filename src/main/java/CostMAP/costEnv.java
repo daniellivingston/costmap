0715ff3a87e5a7a3fa55c7356cf30d023e019b65
Daniel Livingston <daniel.livingston@live.com>
2021-11-12 00:12:51 -0700

    Added some debugging println

--- src/main/java/CostMAP/costEnv.java
+++ src/main/java/CostMAP/costEnv.java
@@ -1,8 +1,10 @@
 package cost_MAP;
 
 import java.io.File;
+import java.io.*;
 
 public class costEnv {
+    private static boolean debugMode = true;
     private static String datasetDir;
     private static String outputDir;
 
@@ -15,8 +17,19 @@ public class costEnv {
     private static String rivers = "rivers.asc";
     private static String roads = "roads.asc";
     private static String slope = "slope.asc";
+    
+    private static void printDebug(String s) {
+        if (debugMode) {
+            System.out.println(s);
+        }
+    }
+    
+    public static void toggleDebug(boolean mode) {
+        debugMode = mode;
+    }
 
     public static void setDatasetDir(String dir) {
+        printDebug("Set: dataset dir = " + dir);
         datasetDir = dir;
     }
 
@@ -29,34 +42,44 @@ public class costEnv {
     }
 
     public static String getAspectPath() {
+        printDebug("Query: aspect path = " + findPath(aspect));
         return findPath(aspect);
     }
     public static String getFedPath() {
+        printDebug("Query: fed path = " + findPath(fed));
         return findPath(fed);
     }
     public static String getLandcoverPath() {
+        printDebug("Query: landcover path = " + findPath(landcover));
         return findPath(landcover);
     }
     public static String getPipelinesPath() {
+        printDebug("Query: pipelines path = " + findPath(pipelines));
         return findPath(pipelines);
     }
     public static String getPopulationPath() {
+        printDebug("Query: population path = " + findPath(population));
         return findPath(population);
     }
     public static String getRailroadsPath() {
+        printDebug("Query: railroads path = " + findPath(railroads));
         return findPath(railroads);
     }
     public static String getRiversPath() {
+        printDebug("Query: rivers path = " + findPath(rivers));
         return findPath(rivers);
     }
     public static String getRoadsPath() {
+        printDebug("Query: roads path = " + findPath(roads));
         return findPath(roads);
     }
     public static String getSlopePath() {
+        printDebug("Query: slope path = " + findPath(slope));
         return findPath(slope);
     }
 
     public static String getOutputPath() {
+        printDebug("Query: output path = " + outputDir);
         return outputDir;
     }
 }
